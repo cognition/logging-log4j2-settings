d8bc011c35ee0709b9d3fb5b5a5d8bf705058bdc
Ramon Brooker <rbrooker@aeo3.io>
2026-02-27 14:44:21 -0500

    starting over

--- hadoop-conf/hdfs-env.sh
+++
@@ -1,6 +0,0 @@
-# Enable HDFS audit logging for SIEM (overrides log4j default NullAppender)
-export HDFS_AUDIT_LOGGER=INFO,RFAAUDIT
-
-# -Dhadoop.root.logger=INFO,RFA routes logs to file (LOG_DIR) instead of console
-export HDFS_NAMENODE_OPTS="-Dhadoop.root.logger=INFO,RFA -Dhdfs.audit.logger=INFO,RFAAUDIT -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=1028"
-export HDFS_DATANODE_OPTS="-Dhadoop.root.logger=INFO,RFA -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=1029"
--- hadoop-conf/mapred-env.sh
+++
@@ -1,4 +0,0 @@
-# Enable MapReduce JobHistory audit (uncomment when running MapReduce jobs)
-# export MAPRED_HISTORYSERVER_OPTS="${MAPRED_HISTORYSERVER_OPTS:-} -Dmapreduce.hs.audit.logger=INFO"
-# -Dhadoop.root.logger=INFO,RFA routes logs to file (LOG_DIR) instead of console
-export MAPRED_HISTORYSERVER_OPTS="-Dhadoop.root.logger=INFO,RFA -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=1030"
